091bf7387bf761a376a2d84319203bc2f73aed31
arturstef <stefanczyk42@gmail.com>
2025-02-03 10:52:31 +0100

    adding new background

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from simulation.simulation import GridSimulation
 
 if __name__ == "__main__":
     sim = GridSimulation(
-        background_path="./fotos/background.png",
+        background_path="./fotos/background2.png",
+        car_image_path="./fotos/car2.png",
         scale_factor=35,  # Bigger cars (10 pixels each)
         fps=10
     )
